1f517cbd09cb265e4251101e61bc4b277d10bd65
RobertoSamael <skateangel99@gmail.com>
2023-09-28 17:02:09 -0500

    label update

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,8 +3,9 @@ import React from 'react'
 export default function Nav() {
   return (
     <nav className='w-full h-fit flex md:flex-row flex-col justify-between items-center px-10 my-4 md:my-7 gap-4'>
-        <div className='h-full flex justify-center items-center'>
+        <div className='h-full flex flex-col justify-center items-center'>
           <h2 className='text-white font-title text-6xl'>F2Random</h2>
+          <h2 className='text-white font-body text-base'>30 Random free games for you to play!</h2>
         </div>
         <div className='w-40 h-full flex flex-col justify-center items-center'>
           <h2 className='text-white font-body text-sm'>Want More?</h2>
